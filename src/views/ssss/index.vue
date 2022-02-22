ee784f4f5483afbd74c63c69cb9db324350a11af
2892441497@qq.com <2892441497@qq.com>
2022-02-22 11:12:19 +0800

    fix(*): sss

--- src/views/ssss/index.vue
+++ src/views/ssss/index.vue
@@ -2,7 +2,7 @@
   <div class="dialog">
     <!--  1.讲解自定义指令、 2.实现防抖自定义指令 3、作用是用来防止搜索的时候导致请求接口过于频繁 ddddd -->
     <input v-model="value" v-anti-shake:none="getData" type="text">
-    <div>ssss</div>
+    <div>sssssssssdasdasdsadasdasdas</div>
   </div>
 </template>
 
